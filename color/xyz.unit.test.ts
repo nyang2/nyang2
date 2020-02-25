01e8d7caae76e4f0d5e6108fe95b49b9a12a8174
임창진 <cjrim@bitnine.net>
2020-02-25 23:39:01 +0900

    #11
    - test case 추가및 수정

--- color/xyz.unit.test.ts
+++ color/xyz.unit.test.ts
@@ -1,22 +1,55 @@
 import {instanceOfXYZ, XYZ} from "./xyz"
+import {instanceOfYxy, Yxy} from "./xyz"
 
-test("XYZ color from numbers (0,0,0)", () => {
-    let color = XYZ(0,0,0);
-    expect(color.X).toBe(0);
-    expect(color.Y).toBe(0);
-    expect(color.Z).toBe(0);
+test("XYZ color from numbers ", () => {
+    let color = XYZ(1/3,1/3,1/3);
+    expect(color.X).toBe(1/3);
+    expect(color.Y).toBe(1/3);
+    expect(color.Z).toBe(1/3);
     expect(instanceOfXYZ(color)).toBe(true);
 });
 
-test("XYZ color from number array [0,0,0]", () => {
+test("XYZ color from number array ", () => {
     let color = XYZ([0,0,0]);
-    expect(color.X).toBe(0);
-    expect(color.Y).toBe(0);
-    expect(color.Z).toBe(0);
+    expect(color.X).toBe(1/3);
+    expect(color.Y).toBe(1/3);
+    expect(color.Z).toBe(1/3);
     expect(instanceOfXYZ(color)).toBe(true);
 });
 
 test("XYZ color from Yxy color", () => {
+    let color = XYZ({Y:0.3, x:0.3, y:0.3, otherAttr:'hello world!'});
+    expect(color.X).toBe(.5);
+    expect(color.Y).toBe(.5);
+    expect(color.Z).toBe(4);
+    expect(instanceOfXYZ(color)).toBe(true);
+});
+
+test("XYZ color from XYZ color", () => {
+    let param = {X:1/3, Y:1/3, Z:1/3, otherAttr:[]};
+    let color = XYZ(param);
+    expect(color).toBe(param);
+});
+
+//------
+
+test("Yxy color from numbers ", () => {
+    let color = Yxy(1/3, 1/3,1/3);
+    expect(color.Y).toBe(1/3);
+    expect(color.x).toBe(1/3);
+    expect(color.y).toBe(1/3);
+    expect(instanceOfXYZ(color)).toBe(true);
+});
+
+test("Yxy color from number array ", () => {
+    let color = Yxy([1/3,1/3,1/3]);
+    expect(color.X).toBe(0);
+    expect(color.Y).toBe(0);
+    expect(color.Z).toBe(0);
+    expect(instanceOfXYZ(color)).toBe(true);
+});
+
+test("Yxy color from XYZ color", () => {
     let color = XYZ({Y:0.5, x:.1, y:.1, otherAttr:'hello world!'});
     expect(color.X).toBe(.5);
     expect(color.Y).toBe(.5);
@@ -24,4 +57,9 @@ test("XYZ color from Yxy color", () => {
     expect(instanceOfXYZ(color)).toBe(true);
 });
 
+test("Yxy color from Yxy color", () => {
+    let param = {X:1/3, Y:1/3, Z:1/3, otherAttr:[]};
+    let color = XYZ(param);
+    expect(color).toBe(param);
+});
 
