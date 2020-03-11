8af42df9561a4688fe91dce2ff612c04db9875ff
easylogic <cyberuls@gmail.com>
2020-03-11 23:48:48 +0900

    범위를 넘어간 값 테스트

--- test/color/rgb2xyz.ts
+++ test/color/rgb2xyz.ts
@@ -6,3 +6,10 @@ test("test rgb2xyz - convert rgb to xyz", () => {
 
     expect(xyz).toEqual({ x: 18.42533039631215, y: 7.784841573295703, z: 95.13924336021469 });
 });  
+
+test("test rgb2xyz - convert rgb to xyz with wrong value(b: 300) ", () => {
+
+    const xyz = rgb2xyz({ r: 10, g: 20, b: 300 });
+
+    expect(xyz).toEqual({ x: 18.42533039631215, y: 7.784841573295703, z: 95.13924336021469 });
+});  
