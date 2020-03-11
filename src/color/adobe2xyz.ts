ac9f37903d1d66381db409ae27a5aca6be80b05a
easylogic <cyberuls@gmail.com>
2020-03-12 00:01:38 +0900

    테스트 코드 추가

--- src/color/adobe2xyz.ts
+++ src/color/adobe2xyz.ts
@@ -1,11 +1,16 @@
 import { RGB, XYZ } from "./color";
+import { minmax } from "../util/math";
+
+function convertColor (colorValue: number) {
+    return Math.pow(( minmax(0, 255, colorValue) / 255 ), 2.19921875) * 100;
+}
 
 export function adobe2xyz (rgb: RGB): XYZ {
     const {r, g, b} = rgb;
 
-    const tempR = Math.pow(( r / 255 ), 2.19921875) * 100;
-    const tempG = Math.pow(( g / 255 ), 2.19921875) * 100;
-    const tempB = Math.pow(( b / 255 ), 2.19921875) * 100;
+    const tempR = convertColor(r)
+    const tempG = convertColor(g)
+    const tempB = convertColor(b)
 
     return {
         x: tempR * 0.57667 + tempG * 0.18555 + tempB * 0.18819,
--- src/color/xyz2adobe.ts
+++ src/color/xyz2adobe.ts
@@ -1,4 +1,5 @@
 import { RGB, XYZ } from "./color";
+import { minmax } from "../util/math";
 
 export function xyz2adobe (xyz: XYZ): RGB {
     const {x, y, z} = xyz;
@@ -12,8 +13,8 @@ export function xyz2adobe (xyz: XYZ): RGB {
     const tempB = Math.pow(tempX *  0.01345 + tempY * -0.11839 + tempZ *  1.01541, ( 1 / 2.19921875 ));
     
     return {
-        r: tempR * 255,
-        g: tempG * 255,
-        b: tempB * 255,   
+        r: minmax(0, 255, Math.round(tempR * 255)),
+        g: minmax(0, 255, Math.round(tempG * 255)),
+        b: minmax(0, 255, Math.round(tempB * 255)),   
     }
 }
