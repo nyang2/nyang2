1a62a0c30cbbb9ccfd1861a4d106f8abdf977f9e
easylogic <cyberuls@gmail.com>
2020-03-11 23:45:58 +0900

    불 필요한 테스트 코드 삭제

--- test/color/rgb2xyz.ts
+++ test/color/rgb2xyz.ts
@@ -6,13 +6,3 @@ test("test rgb2xyz - convert rgb to xyz", () => {
 
     expect(xyz).toEqual({ x: 18.42533039631215, y: 7.784841573295703, z: 95.13924336021469 });
 });  
-
-
-test("test rgb2xyz - wrong parameter", () => {
-    try {
-        rgb2xyz({ r: 10, g: 20, b: 300 });
-    } catch (e) {
-        expect(e.message).toBe('b is must in between 0 and 255');
-    }
-});  
-
